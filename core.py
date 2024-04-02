d6effe3ff9b96f56576972750572d46291d444b8
moonlxight <furkandincer444@gmail.com>
2024-04-02 18:18:40 +0300

    Update core.py

--- core.py
+++ core.py
@@ -3,6 +3,7 @@ import sys
 import requests
 import random
 import colorama
+import art
 import re
 from os import system, name
 from sys import stdout, stderr
@@ -13,4 +14,5 @@ charList = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
 newMac = ""
 
 system("title TwistyJead's Mac Address Changer Tool")
-stderr.writelines("""""")
+stderr.writelines(art.art_mac_address_changer_tool)
+input()
